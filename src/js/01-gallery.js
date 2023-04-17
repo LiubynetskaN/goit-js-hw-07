b2313d8949cda9c5838e9b34f1c83b2f5cd16bc1
Nataliia Liubynetska <nliubynetska@gmail.com>
2023-04-17 18:22:09 +0300

    HW #7 part 1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,42 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const ulEl = document.querySelector(".gallery");
+ulEl.addEventListener("click", openModalHandler);
+
+const imageList = galleryItems.map((galleryItem) => {
+  const element = `<li class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+    />
+  </a>
+</li>`;
+  return element;
+});
+
+const string = imageList.join("");
+ulEl.insertAdjacentHTML("beforeend", string);
+
+function openModalHandler(event) {
+  event.preventDefault();
+  const instance = basicLightbox
+    .create(
+      `<img width="1400" height="900" src="${event.target.dataset.source}">`
+    )
+    instance.show();
+ 
+    document.addEventListener("keydown", closeModalHandler);
+  function closeModalHandler(event) {
+    
+    if (event.code === "Escape") {
+      instance.close();
+    }
+document.removeEventListener("keydown", closeModalHandler)
+  }
+}
